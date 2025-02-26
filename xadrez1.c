b03b285d6efc3b42d615f7c1bca0705345b8a459
milenecs <milene7profissional@gmail.com>
2025-02-26 13:49:24 +0000

    cavalo

--- xadrez1.c
+++ xadrez1.c
@@ -7,10 +7,23 @@ void bt(int casa) {  ///bispo e torre
     printf("Assim cai, vamos tentar de novo  \n");}}
 void rainha(int casas) {
     if (casas >= 0 && casas <= 8 && casas-0){
-    printf("%d Esquerda \n", casas);
+    printf("%d Esquerda \n", casas);           
     rainha(casas-1);
     }else if (casas <0 || casas >=9) {
     printf("Assim ela cai, vamos tentar de novo \n");}}
+void cavalo(){
+    int casasc, casasd;
+    printf("Quantas casas para cima? (1 ou 2)\n");
+    scanf("%d", &casasc);
+    printf("Quantas casas para a direita? (1 ou 2)\n");
+    scanf("%d", &casasd);
+    if (casasc == 1 && casasd == 2){
+    printf("1 cima \n1 Direita \n2 Direita \n");
+    } else if (casasc == 2 && casasd == 1){
+    printf("1 cima \n2 cima \n1 Direita \n");}
+    else {
+        printf("Assim nao vai, vamos tentar de novo \n");}
+}
 int main(){
     int jogador; 
     do{
@@ -47,24 +60,8 @@ int main(){
     rainha(casas);
     break;
     case 4:
-    printf("Quantas Casas Voce Gostaria De Mover o Cavalo para cima? \n");
-    scanf("%d", &casas);
-    for (int i = 1; i <= casas; i++) {  
-    if (i == 1){
-    printf("Cavalo moveu %d casas para cima\n", i);
-    }else if (i == 2){
-    printf ("Cavalo moveu %d casas para cima\n", i);}
-    else if (i < 0 || i >= 3){
-    printf ("Sem roubar, Sao no maximo 2 casa");}}
-    printf("Quantas Casas Voce Gostaria De Mover o Cavalo para a direita? \n");
-    scanf("%d", &casas);
-    for (int i = 1; i <= casas; i++) {
-    if (i == 1){ 
-    printf("Cavalo moveu %d casas para direita\n", i);
-    }else if (i == 2)
-    {printf ("Cavalo moveu %d casas para direita\n", i);}
-    else if (i < 0 || i >= 3){
-    printf ("Sem roubar, Sao no maximo 2 casa");}}
+    printf("Quantas Casas Voce Gostaria De Mover o Cavalo ");
+    cavalo();
     break;
     }
     } while (jogador != 5);
@@ -72,8 +69,8 @@ int main(){
     break;
     case 2:
     printf("** As Regras do jogo sao:\n O Bispo pode se mover ate 5 casas nas diagonais.\n A Torre pode se mover ate 5 na horizontal e vertical.\n A Rainha pode se mover 8 casa e em qualquer direcao.\n");
-    printf(" O Cavalo se movimenta em L, uma casa para cima ou para baixo na vertical e \n mais de duas casas na horizontal OU duas casas para cima ou para baixo na vertical e mais uma casa na horizontal\n ");
-    printf ("Digite 3 para voltar\n");
+    printf(" O Cavalo se movimenta em L, uma casa para cima ou para baixo na vertical e mais de duas casas na horizontal  \n OU duas casas para cima ou para baixo na vertical e mais uma casa na horizontal\n ");
+    printf ("**Digite 3 para voltar**\n");
     scanf("%d", &casas);
     break;
     case 3:
